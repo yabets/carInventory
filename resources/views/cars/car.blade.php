8c0d2fc9f1e4343f3ed6d7144f26354f97af9db1
u2344 <yabetsbelay@gmail.com>
2017-03-25 13:24:36 +0300

    filter for cars done, owners left

--- resources/views/cars/car.blade.php
+++ resources/views/cars/car.blade.php
@@ -3,26 +3,78 @@
 @section('search_page','/index.php/cars/search')
 
 @section('filters')
-    <form class="form-inline" >
+    <form class="form-inline" role="form" action="/index.php/cars/filter" method="post">
+        <input type="hidden" name="_token" value="{{ csrf_token() }}">
         <div class="jumbotron form-inline">
-            <div class="col-lg-2">
+            <div class="col-lg-3">
                 <div class="form-group">
-                    <label>Brand</label>
-                    <input class="form-control" name="brand" placeholder="brand">
+                    <label for="brand">Brand</label>
+                    <select class="form-control" id="brand" name="brand">
+                        <option></option>
+                        <option value="Toyota">Toyota</option>
+                        <option value="Mercedes-Benz">Mercedes-Benz</option>
+                        <option value="BMW">BMW</option>
+                    </select>
                 </div>
             </div>
             {{--col-lg-2--}}
             <div class="col-lg-2">
                 <div class="form-group">
-                    <label>Name</label>
-                    <input class="form-control" name="name" placeholder="name">
+                    <label for="name">Name</label>
+                    <select class="form-control" id="name" name="name">
+                        <option></option>
+                        <option value="Corolla">Corolla</option>
+                        <option value="Hilux">Hilux</option>
+                        <option value="Vitz">Vitz</option>
+                    </select>
                 </div>
             </div>
             {{--col-lg-2--}}
             <div class="col-lg-2">
                 <div class="form-group">
-                    <label>Year</label>
-                    <input class="form-control" name="year" placeholder="year">
+                    <label for="year">Year</label>
+                    <select class="form-control" id="year" name="year">
+                        <option></option>
+                        <option value="1980">1980</option>
+                        <option value="1981">1981</option>
+                        <option value="1982">1982</option>
+                        <option value="1983">1983</option>
+                        <option value="1984">1984</option>
+                        <option value="1985">1985</option>
+                        <option value="1986">1986</option>
+                        <option value="1987">1987</option>
+                        <option value="1988">1988</option>
+                        <option value="1989">1989</option>
+                        <option value="1990">1990</option>
+                        <option value="1991">1991</option>
+                        <option value="1992">1992</option>
+                        <option value="1993">1993</option>
+                        <option value="1994">1994</option>
+                        <option value="1995">1995</option>
+                        <option value="1996">1996</option>
+                        <option value="1997">1997</option>
+                        <option value="1998">1998</option>
+                        <option value="1999">1999</option>
+                        <option value="2000">2000</option>
+                        <option value="2001">2001</option>
+                        <option value="2002">2002</option>
+                        <option value="2003">2003</option>
+                        <option value="2004">2004</option>
+                        <option value="2005">2005</option>
+                        <option value="2006">2006</option>
+                        <option value="2007">2007</option>
+                        <option value="2008">2008</option>
+                        <option value="2009">2009</option>
+                        <option value="2010">2010</option>
+                        <option value="2011">2011</option>
+                        <option value="2012">2012</option>
+                        <option value="2013">2013</option>
+                        <option value="2014">2014</option>
+                        <option value="2015">2015</option>
+                        <option value="2016">2016</option>
+                        <option value="2017">2017</option>
+                        <option value="2018">2018</option>
+                    </select>
                 </div>
             </div>
             {{--col-lg-2--}}
@@ -40,15 +92,29 @@
 
             <div class="col-lg-2">
                 <div class="form-group">
-                    <label>Color</label>
-                    <input class="form-control" name="color" placeholder="color">
+                    <label for="color">Color</label>
+                    <select class="form-control" id="color" name="color">
+                        <option></option>
+                        <option value="white">White</option>
+                        <option value="silver">Silver</option>
+                        <option value="black">Black</option>
+                        <option value="grey">Grey</option>
+                        <option value="blue">Blue</option>
+                        <option value="red">Red</option>
+                        <option value="brown">Brown</option>
+                        <option value="green">Green</option>
+                    </select>
                 </div>
             </div>
             {{--col-lg-2--}}
-            <div class="col-lg-2">
+            <div class="col-lg-3">
                 <div class="form-group">
-                    <label>Transmission</label>
-                    <input class="form-control" name="transmission" placeholder="Transmission">
+                    <label for="transmission">Transmission</label>
+                    <select class="form-control" id="transmission" name="transmission">
+                        <option></option>
+                        <option>manual</option>
+                        <option>automatic</option>
+                    </select>
                 </div>
             </div>
             {{--col-lg-2--}}
@@ -69,18 +135,20 @@
                     <label for="status">Status</label>
                     <select class="form-control" id="status" name="status">
                         <option></option>
-                        <option>avaliable</option>
-                        <option>unavalible</option>
-                        <option>sold</option>
+                        <option value="available">available</option>
+                        <option value="unavailable">unavailable</option>
+                        <option value="sold">sold</option>
                     </select>
-                    <input class="form-control" name="status" placeholder="Status">
                 </div>
             </div>
             {{--col-lg-2--}}
             <div class="col-lg-2">
                 <div class="form-group">
-                    <label>Owner</label>
-                    <input class="form-control" name="owner" placeholder="Owner">
+                    <label for="owner">Owner</label>
+                    <select class="form-control" id="owner" name="owner">
+                        <option></option>
+                        <option value="1">Abebe</option>
+                    </select>
                 </div>
             </div>
             {{--col-lg-2--}}
--- resources/views/cars/create.blade.php
+++ resources/views/cars/create.blade.php
@@ -18,35 +18,83 @@
         <div class="row">
             <div class="col-lg-6">
                 <div class="form-group">
-                    <label>Brand</label>
-                    <input class="form-control" name="brand" placeholder="brand">
-                    <p class="help-block">Example Toyota, Mercedes-Benz, BMW</p>
+                    <label for="brand">Brand</label>
+                    <select class="form-control" id="brand" name="brand">
+                        <option value="Toyota">Toyota</option>
+                        <option value="Mercedes-Benz">Mercedes-Benz</option>
+                        <option value="BMW">BMW</option>
+                    </select>
                 </div>
                 <div class="form-group">
-                    <label>Model Name</label>
-                    <input class="form-control" name="name" placeholder="name">
-                    <p class="help-block">Example Corolla, Hilux, Vitz, Platz, Yaris</p>
+                    <label for="name">Name</label>
+                    <select class="form-control" id="name" name="name">
+                        <option value="Corolla">Corolla</option>
+                        <option value="Hilux">Hilux</option>
+                        <option value="Vitz">Vitz</option>
+                    </select>
                 </div>
                 <div class="form-group">
-                    <label>Year</label>
-                    <input class="form-control" name="year" placeholder="year">
-                    <p class="help-block">Example 1992, 2004, 2015 </p>
+                    <label for="year">Year</label>
+                    <select class="form-control" id="year" name="year">
+                        <option value="1980">1980</option>
+                        <option value="1981">1981</option>
+                        <option value="1982">1982</option>
+                        <option value="1983">1983</option>
+                        <option value="1984">1984</option>
+                        <option value="1985">1985</option>
+                        <option value="1986">1986</option>
+                        <option value="1987">1987</option>
+                        <option value="1988">1988</option>
+                        <option value="1989">1989</option>
+                        <option value="1990">1990</option>
+                        <option value="1991">1991</option>
+                        <option value="1992">1992</option>
+                        <option value="1993">1993</option>
+                        <option value="1994">1994</option>
+                        <option value="1995">1995</option>
+                        <option value="1996">1996</option>
+                        <option value="1997">1997</option>
+                        <option value="1998">1998</option>
+                        <option value="1999">1999</option>
+                        <option value="2000">2000</option>
+                        <option value="2001">2001</option>
+                        <option value="2002">2002</option>
+                        <option value="2003">2003</option>
+                        <option value="2004">2004</option>
+                        <option value="2005">2005</option>
+                        <option value="2006">2006</option>
+                        <option value="2007">2007</option>
+                        <option value="2008">2008</option>
+                        <option value="2009">2009</option>
+                        <option value="2010">2010</option>
+                        <option value="2011">2011</option>
+                        <option value="2012">2012</option>
+                        <option value="2013">2013</option>
+                        <option value="2014">2014</option>
+                        <option value="2015">2015</option>
+                        <option value="2016">2016</option>
+                        <option value="2017">2017</option>
+                        <option value="2018">2018</option>
+                    </select>
                 </div>
                 <div class="form-group">
-                    <label>Color</label>
-                    <input class="form-control" name="color" placeholder="color">
-                    <p class="help-block">Example black, silver, gray, red</p>
+                    <label for="color">Color</label>
+                    <select class="form-control" id="color" name="color">
+                        <option value="white">White</option>
+                        <option value="silver">Silver</option>
+                        <option value="black">Black</option>
+                        <option value="grey">Grey</option>
+                        <option value="blue">Blue</option>
+                        <option value="red">Red</option>
+                        <option value="brown">Brown</option>
+                        <option value="green">Green</option>
+                    </select>
                 </div>
                 <div class="form-group">
                     <label>Price</label>
-                    <input class="form-control" name="price " placeholder="100,000">
-                    <p class="help-block">Example 100,000 250,000 1,500,000</p>
+                    <input class="form-control" type="number" min="0" name="price" placeholder="100000">
                 </div>
-                <div class="form-group">
-                    <label>Transmission</label>
-                    <input class="form-control" name="transmission" placeholder="transmission">
-                    <p class="help-block">Example manual, automatic </p>
-                </div>
+
                 <div class="form-group">
                     <label>Plate</label>
                     <input class="form-control" name="plate" placeholder="plate">
@@ -66,19 +114,27 @@
                     <input type="radio" name="meri" value="yezore">yezore
                 </div>
                 <div class="form-group">
+                    <label for="transmission">Transmission</label>
+                    <input type="radio" name="transmission" value="manual" checked>manual
+                    <input type="radio" name="transmission" value="automatic">automatic
+                </div>
+                <div class="form-group">
                     <label>Mileage</label>
-                    <input class="form-control" name="mileage " placeholder="100,000">
+                    <input class="form-control" type="number" min="0" name="mileage " placeholder="100,000">
                     <p class="help-block">Example 100,000 250,000 959,900</p>
                 </div>
                 <div class="form-group">
                     <label>Status</label>
-                    <input class="form-control" name="status" value="available">
-                    <p class="help-block">Example sold, available, not available </p>
+                    <input type="radio" name="status" value="available" checked>available
+                    <input type="radio" name="status" value="unavailable">unavailable
+                    <input type="radio" name="status" value="sold">sold
                 </div>
                 <div class="form-group">
-                    <label>Owner</label>
-                    <input class="form-control" name="owner_id" value="1">
-                    <p class="help-block">Example select owner</p>
+                    <label for="owner">Owner</label>
+                    <select class="form-control" id="owner" name="owner_id">
+                        <option></option>
+                        <option value="1">Abebe</option>
+                    </select>
                 </div>
                 <div class="form-group">
                     <label>Remark</label>
