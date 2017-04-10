4d15c9b031d41c637a933db8709dd887af679996
u2344 <yabetsbelay@gmail.com>
2017-04-10 07:43:45 +0300

    edit requeted car with new request issude found

--- resources/views/callrecords/edit.blade.php
+++ resources/views/callrecords/edit.blade.php
@@ -26,6 +26,18 @@
                     $("#name").find('option').remove().end().append(html);
                 });
             });
+            $('#brandNew').change(function(){
+                selBrand = $('#brand :selected').val();
+                url = "/index.php/param/" + selBrand;
+                $.get(url, function(data){
+                    var res = data.split(",");
+                    html = "<option></option>";
+                    for(var key in res) {
+                        html += "<option value=" + res[key]  + ">" +res[key] + "</option>"
+                    }
+                    $("#nameNew").find('option').remove().end().append(html);
+                });
+            });
         });
     </script>
 
@@ -222,8 +234,103 @@
         </div>
         @endforeach
         @if ($call->requestedCars->isEmpty())
-            <input hiddent name="requestedNew" value="1">
-            <button>New Request</button>
+            <input hidden name="requestedNew" value="1">
+            <div class="row nofound">
+                <h2 class="col-md-3">Requested Car</h2>
+            </div>
+            <div class="row nofound">
+                <div class="form-group col-md-2">
+                    {{ Form::label('brand', 'Brand') }}
+                    <select class="form-control" id="brandNew" name="brandNew">
+                        <option></option>
+                        @foreach($brands as $brand)
+                            <option value="{{$brand}}" >{{$brand}}</option>
+                        @endforeach
+                    </select>
+                </div>
+                <div class="form-group col-md-2">
+                    <label for="name">Name</label>
+                    <select class="form-control" id="nameNew" name="nameNew">
+                        <option></option>
+                    </select>
+                </div>
+                <div class="form-group col-md-2">
+                    <label for="year">Year</label>
+                    <select class="form-control" id="year" name="yearNew">
+                        <option value="1980">1980</option>
+                        <option value="1981">1981</option>
+                        <option value="1982">1982</option>
+                        <option value="1983">1983</option>
+                        <option value="1984">1984</option>
+                        <option value="1985">1985</option>
+                        <option value="1986">1986</option>
+                        <option value="1987">1987</option>
+                        <option value="1988">1988</option>
+                        <option value="1989">1989</option>
+                        <option value="1990">1990</option>
+                        <option value="1991">1991</option>
+                        <option value="1992">1992</option>
+                        <option value="1993">1993</option>
+                        <option value="1994">1994</option>
+                        <option value="1995">1995</option>
+                        <option value="1996">1996</option>
+                        <option value="1997">1997</option>
+                        <option value="1998">1998</option>
+                        <option value="1999">1999</option>
+                        <option value="2000">2000</option>
+                        <option value="2001">2001</option>
+                        <option value="2002">2002</option>
+                        <option value="2003">2003</option>
+                        <option value="2004">2004</option>
+                        <option value="2005">2005</option>
+                        <option value="2006">2006</option>
+                        <option value="2007">2007</option>
+                        <option value="2008">2008</option>
+                        <option value="2009">2009</option>
+                        <option value="2010">2010</option>
+                        <option value="2011">2011</option>
+                        <option value="2012">2012</option>
+                        <option value="2013">2013</option>
+                        <option value="2014">2014</option>
+                        <option value="2015">2015</option>
+                        <option value="2016">2016</option>
+                        <option value="2017">2017</option>
+                        <option value="2018">2018</option>
+                    </select>
+                </div>
+                <div class="form-group col-md-2">
+                    <label for="color">Color</label>
+                    <select class="form-control" id="color" name="colorNew">
+                        @foreach($colors as $color)
+                            <option value="{{$color}}" >{{$color}}</option>
+                        @endforeach
+                    </select>
+                </div>
+                <div class="form-group col-md-3">
+                    <label for="transmission">Transmission</label>
+                    @foreach($transmissions as $transmission)
+                        <input type="radio" name="transmissionNew" value="{{$transmission}}">{{$transmission}}
+                    @endforeach
+                </div>
+            </div>
+            <div class="row nofound">
+                <div class="form-group col-md-4">
+                    <label>Price</label>
+                    <input  name="priceFromNew" placeholder="100000">
+                    <label> - </label>
+                    <input  name="priceToNew" placeholder="400000">
+                </div>
+                <div class="form-group col-md-3">
+                    <label>Plate</label>
+                    <input name="plate" placeholder="plateNew">
+                </div>
+                <input name="status" hidden value="not found" >
+                <div class="form-group col-md-2">
+                    <label>Meri</label>
+                    <input type="radio" name="meriNew" value="yalezore" checked> yalezore
+                    <input type="radio" name="meriNew" value="yezore"> yezore
+                </div>
+            </div>
         @endif
     @endif
         <button type="submit" class="btn btn-default">Submit</button>
